28d47104492193afd8e8c29db2643ecb60109f86
Oscar Escobar <qomc16@gmail.com>
2022-10-26 20:10:55 -0600

    Rol Terminado

--- demo/src/main/java/proyectobd/demo/service/RolService.java
+++ demo/src/main/java/proyectobd/demo/service/RolService.java
@@ -1,10 +1,15 @@
 package proyectobd.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,18 @@ public class RolService {
 	@GetMapping(path = "/buscar")
      public List<Rol> buscar(){
 		return rolRepository.findAll();
+	}
+	
+	@PostMapping(path ="/guardar")
+	public Rol saveRol(@RequestBody Rol rol) {
+		return rolRepository.save(rol);
+	}
+	
+	@DeleteMapping(path="/eliminar/{id_rol}")
+	public void eliminar(@PathVariable ("id_rol") Integer id_rol) {
+		Optional <Rol> rol =  rolRepository.findById(id_rol);
+		if(rol.isPresent()) {
+			rolRepository.delete(rol.get());
+		}
 	}
 }
